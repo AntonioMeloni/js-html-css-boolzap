43005cce02b6a713a7d9b1bf71e1a93127c4555e
Antonio <ameloni9@gmail.com>
2020-03-05 16:09:11 +0100

    Aggiunta la ricerca dei contatti e linvio dei messaggi tramite input

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.vocal').click(function(){
+        var testoMessaggio = $('#message-text').val();
+        $('#message-text').val('');
+        // $('#nome-utente').html('<span class="rosso">' + nomeInput + '</span>');
+        // $('#nome-utente').text('<span class="rosso">' + nomeInput + '</span>');
+        // $('#nome-utente').text(nomeInput).attr('class', 'rosso').css('background-color', 'yellow');
+        // $('.lista-nomi').append('<div>' + nomeInput + '</div>');
+        var messaggio = $('.template .message').clone(); // Copia del contenuto del messaggio che è dentro al template (display none nel nostro CSS)
+        messaggio.children('.send').text(testoMessaggio); // Modifico il testo messaggio nel messaggio
+        messaggio.find('.orario').text('05:05');
+        $('.chat').append(messaggio); // Aggiungo in fondo alla lista nomi il messaggio
+    });
+
+    $('#message-text').click(function () {
+        $('.microphone').hide();
+        $('.invia').show();
+    });
+
+    $('.invia').click(function () {
+        $('.invia').hide();
+        $('.microphone').show();
+    });
+});
+
+$('#cerca-contatti').keyup(function(event){
+    var carattereFiltro = $(this).val().toLowerCase();
+    // console.log(carattereFiltro);
+    $('.contact').each(function(){ // Se nella lista contatti è presente il carattere digitato visualizzarlo
+        // console.log($(this).text());
+        if ($(this).find('.name').text().toLowerCase().includes(carattereFiltro)) { // Se il nome del list item ha al suo interno i caratteri digitati visualizzalo
+            $(this).show();
+        } else { // Altrimenti non visualizzarlo
+            $(this).hide();
+        }
+    });
+});
