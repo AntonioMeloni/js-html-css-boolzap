79248901bc21db72affed1194dc76b3b5916ca58
Antonio <ameloni9@gmail.com>
2020-03-09 12:05:55 +0100

    Aggiunto click su contatto che mostra la relativa conversazione

--- js/main.js
+++ js/main.js
@@ -8,25 +8,26 @@ $(document).ready(function(){
         return time;
     }
 
-    $('.vocal').click(function(){
-        var testoMessaggio = $('#message-text').val();
-        $('#message-text').val('');
-        var messaggio = $('.template-send .message').clone();
-        messaggio.find('.send').text(testoMessaggio);
-        messaggio.find('.orario').text(mostraOra());
-        $('.chat').append(messaggio);
+    $('.vocal').click(function (){
+        sendMessage();
     });
 
-    $('.invia').click(function(){
-        var autoreply = setInterval(function () {
-            var testoMessaggio = 'Ok';
-            var messaggio = $('.template-received .message').clone();
-            messaggio.find('.received').text(testoMessaggio);
-            messaggio.find('.orario').text(mostraOra());
-            $('.chat').append(messaggio);
-            clearInterval(autoreply);
-        },1500);
-    });
+    $('#message-text').keypress(function (event) {
+        if(event.keyCode == 13){
+            sendMessage();
+        }
+    })
+
+    // $('.invia').click(function(){
+    //     var autoreply = setInterval(function () {
+    //         var testoMessaggio = 'Ok';
+    //         var messaggio = $('.template-received .message').clone();
+    //         messaggio.find('.received').text(testoMessaggio);
+    //         messaggio.find('.orario').text(mostraOra());
+    //         $('.chat').append(messaggio);
+    //         clearInterval(autoreply);
+    //     },1500);
+    // });
 
     $('#message-text').click(function () {
         $('.microphone').hide();
@@ -37,16 +38,64 @@ $(document).ready(function(){
         $('.invia').hide();
         $('.microphone').show();
     });
-});
 
-$('#cerca-contatti').keyup(function(event){
-    var carattereFiltro = $(this).val().toLowerCase();
-    ;
-    $('.contacts .contact').each(function(){
-        if ($(this).find('.name').text().toLowerCase().includes(carattereFiltro)) {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
+    $('#cerca-contatti').keyup(function(event){
+        var carattereFiltro = $(this).val().toLowerCase();
+        ;
+        $('.contacts .contact').each(function(){
+            if ($(this).find('.name').text().toLowerCase().includes(carattereFiltro)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
     });
+
+    function sendMessage() {
+        var testoMessaggio = $('#message-text').val();
+        if(testoMessaggio.trim().length > 0) {
+        $('#message-text').val('');
+        var messaggio = $('.template-send .message').clone();
+        messaggio.find('.send').text(testoMessaggio);
+        messaggio.find('.orario').text(mostraOra());
+        $('.chat-container.chat-active').append(messaggio);
+        autoRespond();
+        scroll();
+        }
+    }
+
+    function autoRespond() {
+        var autoreply = setInterval(function () {
+            var testoMessaggio = 'Ok';
+            var messaggio = $('.template-received .message').clone();
+            messaggio.find('.received').text(testoMessaggio);
+            messaggio.find('.orario').text(mostraOra());
+            $('.chat-container.chat-active').append(messaggio);
+            clearInterval(autoreply);
+            scroll();
+        },1500);
+    }
+
+    function scroll() {
+          var pixelScroll = $('.chat-container').height();
+          $('.chat-container.chat-active').scrollTop(pixelScroll);
+     }
+
+     $('.contact').click(function() {
+     var nome = $(this).find('.name').text();
+     $('.contact').removeClass('active');
+      $(this).addClass('active');
+
+     var utente = $(this).data('utente');
+     $('.chat-container').each(function(){
+          if(utente == $(this).data('utente')) {
+              $('.chat-container').removeClass('chat-active');
+               $(this).addClass('chat-active');
+               $('.chat-container').hide();
+               $(this).show();
+          }
+     });
+
+
+    })
 });
